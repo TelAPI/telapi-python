0066c6980fa02ce8008e1013b997d4307b8d4dfa
Matt Williamson <matt@teltechcorp.com>
2012-06-04 11:40:04 -0400

    TelML now pulling from JSON shcema

--- telapi/telml/__init__.py
+++ telapi/telml/__init__.py
@@ -1,18 +1,31 @@
 from xml.sax.saxutils import escape
+import json
+import os
+from new import classobj
 
-class Response(object):
+SCHEMA_FILE = os.path.join(os.path.dirname(__file__), '../schema/telapi.json')
+SCHEMA = json.load(open(SCHEMA_FILE))
+
+class Element(object):
     """Root XML element. Also, base class for all other TelML elements"""
 
     _allowed_attributes = []
-    _allowed_children = ['Say', 'Play', 'Gather', 'Record', 'Dial']
-    _requires_body = False
+    _allowed_children   = []
+    _allow_blank        = False
 
     def __init__(self, *args, **kwargs):
         object.__init__(self)
+
         self._element_name = self.__class__.__name__.split('.')[-1]
-        self._body = ''
-        self._attributes = {}
-        self._children = []
+        self._body         = ''
+        self._attributes   = {}
+        self._children     = []
+
+        for i, arg in enumerate(args):
+            if i == 0 and isinstance(arg, str):
+                self._body = arg
+            elif isinstance(arg, Element):
+                self._children.append(arg)
 
         for k, v in kwargs.items():
             setattr(self, k, v)
@@ -21,24 +34,28 @@ class Response(object):
         return str(unicode(self))
 
     def __unicode__(self):
-        if self._requires_body and not unicode(self._body).strip():
-            raise ValueError('Non-empty "body" property required for the "%s" element!' % self._element_name)
-
         attribute_string = ''
         body_string = ''.join([unicode(child) for child in self._children]) or self._body
 
+        if not self._allow_blank and not body_string.strip():
+            raise ValueError('The "%s" element cannot be blank!' % 
+                self._element_name)
+
         if len(self._attributes):
-            attribute_string = ' ' + ' '.join(['%s="%s"' % (escape(unicode(k)), escape(unicode(v))) for k, v in self._attributes.items()])
+            attribute_string = ' ' + ' '.join(['%s="%s"' % (escape(unicode(k)), escape(unicode(v))) 
+                for k, v in self._attributes.items()])
 
         return u"<%s%s>%s</%s>" % (self._element_name, attribute_string, body_string, self._element_name)
 
     def _ensure_attribute(self, name):
         if name not in self._allowed_attributes:
-            raise AttributeError('"%s" is not a valid attribute of the "%s" element!' % (name, self._element_name))
+            raise AttributeError('"%s" is not a valid attribute of the "%s" element!' % 
+                (name, self._element_name))
 
     def _ensure_child(self, name):
         if name not in self._allowed_children:
-            raise TypeError('"%s" is not a valid child element of the "%s" element!' % (name, self._element_name))
+            raise TypeError('"%s" is not a valid child element of the "%s" element!' % 
+                (name, self._element_name))
 
     def __setattr__(self, name, value):
         if name.startswith('_'):
@@ -72,56 +89,26 @@ class Response(object):
         self._ensure_child(child._element_name)
         self._children.append(child)
 
-class Say(Response):
-    _allowed_children = []
-    _allowed_attributes = ['voice', 'loop']
-    _requires_body = True
 
-    def __init__(self, body, *args, **kwargs):
-        Response.__init__(self, *args, **kwargs)
-        self._body = body
 
-class Play(Say):
-    _allowed_attributes = ['loop']
+# Loop through the schema file and dynamically create classes for each element type
+for element, properties in SCHEMA['telml']['verbs'].items():
+    docstring = """%s element
+    -------------------------
+    Nestable children: %s
+    More info: %s
+    """ % (
+        element, 
+        map(str, properties['nesting']), 
+        properties['docs_url']
+    )
 
-class Pause(Response):
-    _allowed_children = []
-    _allowed_attributes = ['length']
+    element_dict = {
+        '_allowed_attributes'   : properties['attributes'],
+        '_allowed_children'     : properties['nesting'],
+        '_allow_blank'          : properties['blank'],
+        '__doc__'               : docstring,
+    }
+    globals()[str(element)] = classobj(str(element), (Element,), element_dict)
 
-class Gather(Response):
-    _allowed_children = ['Say', 'Play', 'Pause']
-    _allowed_attributes = ['action', 'method', 'timeout', 'finishOnKey', 'numDigits']
-
-    def __init__(self, *args, **kwargs):
-        # TODO: Check if a a child element is required or if gather can sit by itself
-        Response.__init__(self, *args, **kwargs)
-        
-        for child in args:
-            self.append(child)
-
-class Record(Response):
-    _allowed_children = []
-    _allowed_attributes = ['action', 'method', 'timeout', 'finishOnKey', 'transcribe', 'transcribeCallback', 'playBeep', 'bothLegs', 'fileFormat']
-
-class Number(Say):
-    _allowed_attributes = ['sendDigits', 'method', 'url']
-
-class Conference(Say):
-    _allowed_attributes = ['muted', 'beep', 'startConferenceOnEnter', 'endConferenceOnExit', 'maxParticipants', 'waitUrl', 'waitMethod', 
-        'hangupOnStar', 'callbackUrl', 'method', 'waitSound', 'waitSoundMethod', 'digitsMatch', 'stayAlone']
-
-class Dial(Response):
-    def __init__(self, body='', *args, **kwargs):
-        Response.__init__(self, *args, **kwargs)
-
-        if isinstance(body, Number) or isinstance(body, Conference):
-            self._children.append(body)
-            self._body = True
-        else:
-            self._body = body
-
-    _requires_body = True
-    _allowed_attributes = ['action', 'method', 'timeout', 'hangupOnStar', 'timeLimit', 'callerId', 'hideCallerId', 'callerName', 'dialMusic', 
-    'callbackUrl', 'callbackMethod', 'confirmSound', 'digitsMatch', 'straightToVm', 'heartbeatUrl', 'heartbeatMethod', 'forwardedFrom']
-    _allowed_children = ['Number', 'Conference']
 
--- tests/test_telml.py
+++ tests/test_telml.py
@@ -1,72 +1,73 @@
 import unittest
-import telapi.telml as tml
+import telapi.telml as telml
 
 class TestAllTelml(unittest.TestCase):
     def setUp(self):
-        self.response = tml.Response()
+        self.response = telml.Response()
 
 
 
     # Response
     def test_response(self):
-        self.assertEqual(str(self.response), '<Response></Response>')
+        with self.assertRaises(ValueError):
+            self.assertEqual(str(self.response), '<Response></Response>')
 
 
 
     # Say
     def test_say_no_body(self):
-        with self.assertRaises(TypeError):
-            tml.Say()
+        with self.assertRaises(ValueError):
+            str(telml.Say())
 
     def test_bad_child(self):
         with self.assertRaises(TypeError):
-            say = tml.Say('Hi')
-            say.append(tml.Say("there"))
+            say = telml.Say('Hi')
+            say.append(telml.Say("there"))
 
     def test_say_body(self):
-        self.response.append(tml.Say('Hello there'))
+        self.response.append(telml.Say('Hello there'))
         self.assertEqual(str(self.response), '<Response><Say>Hello there</Say></Response>')
 
     def test_say_bad_attributes(self):
         with self.assertRaises(AttributeError):
-            tml.Say('hello', loops=5)
+            telml.Say('hello', loops=5)
 
     def test_say_all_attributes(self):
-        self.response.append(tml.Say('Hello there', voice='man', loop=1))
+        self.response.append(telml.Say('Hello there', voice='man', loop=1))
         self.assertEqual(str(self.response), '<Response><Say voice="man" loop="1">Hello there</Say></Response>')
 
 
 
     # Play
     def test_play_body(self):
-        self.response.append(tml.Play('http://test.url.com/myfile.mp3', loop=1))
+        self.response.append(telml.Play('http://test.url.com/myfile.mp3', loop=1))
         self.assertEqual(str(self.response), '<Response><Play loop="1">http://test.url.com/myfile.mp3</Play></Response>')
 
 
 
     # Gather
     def test_gather(self):
-        self.response.append(tml.Gather())
+        self.response.append(telml.Gather())
         self.assertEqual(str(self.response), '<Response><Gather></Gather></Response>')
 
     def test_gather_attributes(self):
-        self.response.append(tml.Gather(action="http://mysite.com/gather/", method="POST", timeout=10, 
+        self.response.append(telml.Gather(action="http://mysite.com/gather/", method="POST", timeout=10, 
             finishOnKey='true', numDigits=5))
         self.assertEqual(str(self.response), 
             '<Response><Gather finishOnKey="true" action="http://mysite.com/gather/" method="POST" timeout="10" numDigits="5"></Gather></Response>')
 
     def test_gather_bad_attributes(self):
         with self.assertRaises(AttributeError):
-            self.response.append(tml.Gather(action="http://mysite.com/gather/", method="POST", timeout=10, 
+            self.response.append(telml.Gather(action="http://mysite.com/gather/", method="POST", timeout=10, 
                 finishOnKey='true', numdigits=5))
 
     def test_gather_children(self):
         self.response.append(
-            tml.Gather(
-                tml.Say('Enter digits'), 
-                tml.Play('http://test.com/pound.mp3'), 
-                tml.Pause(), 
-                tml.Say("Followed by pound key"),
+            telml.Gather(
+                telml.Say('Enter digits'), 
+                telml.Play('http://test.com/pound.mp3'), 
+                telml.Pause(), 
+                telml.Say("Followed by pound key"),
                 timeout = 30
             )
         )
@@ -79,7 +80,7 @@ class TestAllTelml(unittest.TestCase):
     def test_record_attributes(self):
         # No value validation yet, so just make sure all the attrs are assignable
         record_attrs = dict([(attr, attr) for attr in ['action', 'method', 'timeout', 'finishOnKey', 'transcribe', 'transcribeCallback', 'playBeep', 'bothLegs', 'fileFormat']])
-        self.response.append(tml.Record(**record_attrs))
+        self.response.append(telml.Record(**record_attrs))
         self.assertEqual(
             str(self.response), 
             '<Response><Record finishOnKey="finishOnKey" transcribe="transcribe" playBeep="playBeep" transcribeCallback="transcribeCallback" fileFormat="fileFormat" bothLegs="bothLegs" timeout="timeout" action="action" method="method"></Record></Response>'
@@ -88,37 +89,39 @@ class TestAllTelml(unittest.TestCase):
 
     # Dial
     def test_dial_simple(self):
-        self.response.append(tml.Dial('1-555-222-3456'))
+        self.response.append(telml.Dial('1-555-222-3456'))
         self.assertEqual(str(self.response), '<Response><Dial>1-555-222-3456</Dial></Response>')
 
     def test_dial_number(self):
-        self.response.append(tml.Dial(tml.Number('1-555-222-3456')))
+        self.response.append(telml.Dial(telml.Number('1-555-222-3456')))
         self.assertEqual(
             str(self.response), 
             '<Response><Dial><Number>1-555-222-3456</Number></Dial></Response>'
         )
 
     def test_dial_blank_number(self):
-        with self.assertRaises(TypeError):
-            self.response.append(tml.Dial(tml.Number()))
+        with self.assertRaises(ValueError):
+            self.response.append(telml.Dial(telml.Number()))
+            str(self.response)
 
     def test_dial_attributes(self):
         # No value validation yet, so just make sure all the attrs are assignable
         dial_attrs = dict([(attr, attr) for attr in ['action', 'method', 'timeout', 'hangupOnStar', 'timeLimit', 'callerId', 'hideCallerId', 'callerName', 'dialMusic', 
         'callbackUrl', 'callbackMethod', 'confirmSound', 'digitsMatch', 'straightToVm', 'heartbeatUrl', 'heartbeatMethod', 'forwardedFrom']])
-        self.response.append(tml.Dial("1-555-222-3456", **dial_attrs))
+        self.response.append(telml.Dial("1-555-222-3456", **dial_attrs))
         self.assertEqual(
             str(self.response), 
-            '<Response><Dial hideCallerId="hideCallerId" heartbeatMethod="heartbeatMethod" callerName="callerName" callerId="callerId" confirmSound="confirmSound" digitsMatch="digitsMatch" forwardedFrom="forwardedFrom" callbackMethod="callbackMethod" timeLimit="timeLimit" dialMusic="dialMusic" timeout="timeout" heartbeatUrl="heartbeatUrl" action="action" straightToVm="straightToVm" hangupOnStar="hangupOnStar" method="method" callbackUrl="callbackUrl">1-555-222-3456</Dial></Response>'
+            '<Response><Dial hideCallerId="hideCallerId" heartbeatMethod="heartbeatMethod" callerName="callerName" callerId="callerId" confirmSound="confirmSound" digitsMatch="digitsMatch" timeout="timeout" callbackMethod="callbackMethod" timeLimit="timeLimit" dialMusic="dialMusic" straightToVm="straightToVm" heartbeatUrl="heartbeatUrl" action="action" forwardedFrom="forwardedFrom" hangupOnStar="hangupOnStar" method="method" callbackUrl="callbackUrl">1-555-222-3456</Dial></Response>'
         )
 
     def test_dial_blank_conference(self):
-        with self.assertRaises(TypeError):
-            self.response.append(tml.Dial(tml.Conference()))
+        with self.assertRaises(ValueError):
+            self.response.append(telml.Dial(telml.Conference()))
+            str(self.response)
 
     def test_dial_conference(self):
-        self.response.append(tml.Say('You are about to enter the conference'))
-        self.response.append(tml.Dial(tml.Conference('Conference Room A')))
+        self.response.append(telml.Say('You are about to enter the conference'))
+        self.response.append(telml.Dial(telml.Conference('Conference Room A')))
         self.assertEqual(
             str(self.response), 
             '<Response><Say>You are about to enter the conference</Say><Dial><Conference>Conference Room A</Conference></Dial></Response>'
