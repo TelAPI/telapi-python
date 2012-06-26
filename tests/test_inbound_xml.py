dae5e5622df34d7562a4cfcdd0d6f5456d7f4f77
Matt Williamson <matt@teltechcorp.com>
2012-06-26 10:22:11 -0400

    Telapi-Python helper working well

---
+++ tests/test_inbound_xml.py
@@ -0,0 +1,131 @@
+import unittest
+from telapi import inboundxml
+
+class TestAllTelml(unittest.TestCase):
+    def setUp(self):
+        self.response = inboundxml.Response()
+
+
+
+    # Response
+    def test_response(self):
+        with self.assertRaises(ValueError):
+            self.assertEqual(str(self.response), '<Response></Response>')
+
+
+
+    # Say
+    def test_say_no_body(self):
+        with self.assertRaises(ValueError):
+            str(inboundxml.Say())
+
+    def test_bad_child(self):
+        with self.assertRaises(TypeError):
+            say = inboundxml.Say('Hi')
+            say.append(inboundxml.Say("there"))
+
+    def test_say_body(self):
+        self.response.append(inboundxml.Say('Hello there'))
+        self.assertEqual(str(self.response), '<Response><Say>Hello there</Say></Response>')
+
+    def test_say_bad_attributes(self):
+        with self.assertRaises(AttributeError):
+            inboundxml.Say('hello', loops=5)
+
+    def test_say_all_attributes(self):
+        self.response.append(inboundxml.Say('Hello there', voice='man', loop=1))
+        self.assertEqual(str(self.response), '<Response><Say voice="man" loop="1">Hello there</Say></Response>')
+
+
+
+    # Play
+    def test_play_body(self):
+        self.response.append(inboundxml.Play('http://test.url.com/myfile.mp3', loop=1))
+        self.assertEqual(str(self.response), '<Response><Play loop="1">http://test.url.com/myfile.mp3</Play></Response>')
+
+
+
+    # Gather
+    def test_gather(self):
+        self.response.append(inboundxml.Gather())
+        self.assertEqual(str(self.response), '<Response><Gather></Gather></Response>')
+
+    def test_gather_attributes(self):
+        self.response.append(inboundxml.Gather(action="http://mysite.com/gather/", method="POST", timeout=10, 
+            finishOnKey='true', numDigits=5))
+        self.assertEqual(str(self.response), 
+            '<Response><Gather finishOnKey="true" action="http://mysite.com/gather/" method="POST" timeout="10" numDigits="5"></Gather></Response>')
+
+    def test_gather_bad_attributes(self):
+        with self.assertRaises(AttributeError):
+            self.response.append(inboundxml.Gather(action="http://mysite.com/gather/", method="POST", timeout=10, 
+                finishOnKey='true', numdigits=5))
+
+    def test_gather_children(self):
+        self.response.append(
+            inboundxml.Gather(
+                inboundxml.Say('Enter digits'), 
+                inboundxml.Play('http://test.com/pound.mp3'), 
+                inboundxml.Pause(), 
+                inboundxml.Say("Followed by pound key"),
+                timeout = 30
+            )
+        )
+        self.assertEqual(str(self.response), 
+            '<Response><Gather timeout="30"><Say>Enter digits</Say><Play>http://test.com/pound.mp3</Play><Pause></Pause><Say>Followed by pound key</Say></Gather></Response>')
+
+
+
+    # Record
+    def test_record_attributes(self):
+        # No value validation yet, so just make sure all the attrs are assignable
+        record_attrs = dict([(attr, attr) for attr in ['action', 'method', 'timeout', 'finishOnKey', 'transcribe', 'transcribeCallback', 'playBeep', 'bothLegs', 'fileFormat']])
+        self.response.append(inboundxml.Record(**record_attrs))
+        self.assertEqual(
+            str(self.response), 
+            '<Response><Record finishOnKey="finishOnKey" transcribe="transcribe" playBeep="playBeep" transcribeCallback="transcribeCallback" fileFormat="fileFormat" bothLegs="bothLegs" timeout="timeout" action="action" method="method"></Record></Response>'
+        )
+
+
+    # Dial
+    def test_dial_simple(self):
+        self.response.append(inboundxml.Dial('1-555-222-3456'))
+        self.assertEqual(str(self.response), '<Response><Dial>1-555-222-3456</Dial></Response>')
+
+    def test_dial_number(self):
+        self.response.append(inboundxml.Dial(inboundxml.Number('1-555-222-3456')))
+        self.assertEqual(
+            str(self.response), 
+            '<Response><Dial><Number>1-555-222-3456</Number></Dial></Response>'
+        )
+
+    def test_dial_blank_number(self):
+        with self.assertRaises(ValueError):
+            self.response.append(inboundxml.Dial(inboundxml.Number()))
+            str(self.response)
+
+    def test_dial_attributes(self):
+        # No value validation yet, so just make sure all the attrs are assignable
+        dial_attrs = dict([(attr, attr) for attr in ['action', 'method', 'timeout', 'hangupOnStar', 'timeLimit', 'callerId', 'hideCallerId', 'callerName', 'dialMusic', 
+        'callbackUrl', 'callbackMethod', 'confirmSound', 'digitsMatch', 'straightToVm', 'heartbeatUrl', 'heartbeatMethod', 'forwardedFrom']])
+        self.response.append(inboundxml.Dial("1-555-222-3456", **dial_attrs))
+        self.assertEqual(
+            str(self.response), 
+            '<Response><Dial hideCallerId="hideCallerId" heartbeatMethod="heartbeatMethod" callerName="callerName" callerId="callerId" confirmSound="confirmSound" digitsMatch="digitsMatch" timeout="timeout" callbackMethod="callbackMethod" timeLimit="timeLimit" dialMusic="dialMusic" straightToVm="straightToVm" heartbeatUrl="heartbeatUrl" action="action" forwardedFrom="forwardedFrom" hangupOnStar="hangupOnStar" method="method" callbackUrl="callbackUrl">1-555-222-3456</Dial></Response>'
+        )
+
+    def test_dial_blank_conference(self):
+        with self.assertRaises(ValueError):
+            self.response.append(inboundxml.Dial(inboundxml.Conference()))
+            str(self.response)
+
+    def test_dial_conference(self):
+        self.response.append(inboundxml.Say('You are about to enter the conference'))
+        self.response.append(inboundxml.Dial(inboundxml.Conference('Conference Room A')))
+        self.assertEqual(
+            str(self.response), 
+            '<Response><Say>You are about to enter the conference</Say><Dial><Conference>Conference Room A</Conference></Dial></Response>'
+        )
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_rest.py
@@ -0,0 +1,196 @@
+import unittest
+import os
+import json
+import time
+from telapi import rest
+
+class TestREST(unittest.TestCase):
+    def setUp(self):
+        # Environment variables must be set for TELAPI_ACCOUNT_SID and TELAPI_AUTH_TOKEN
+        self.client = rest.Client(base_url='https://api.dev.telapi.com/2011-07-01/')
+        # self.client = rest.Client()
+        self.test_cell_number = os.environ.get('TELAPI_TEST_NUMBER')
+
+        if not os.environ.get('TELAPI_ACCOUNT_SID'):
+            raise Exception("Please set the TELAPI_ACCOUNT_SID, TELAPI_AUTH_TOKEN and TELAPI_TEST_NUMBER environment variables to run the tests!")
+
+
+    # Test REST Client getattr
+    def test_bad_resource_list_resource(self):
+        with self.assertRaises(AttributeError):
+            self.client.bad_resource_name
+
+    def test_account_list_resource(self):
+        accounts = self.client.accounts
+        self.assertEquals(accounts.__class__.__name__, 'AccountListResource')
+        self.assertEquals(accounts._url, 'Accounts')
+
+        # Make sure the list resource doesn't have instance resource params
+        with self.assertRaises(AttributeError):
+            accounts.sid
+
+        # Bad property name
+        with self.assertRaises(AttributeError):
+            accounts.random_property_name
+
+        # Dict-style key access
+        account = accounts[self.client.account_sid]
+
+        # Check types and generated URLs
+        self.assertEquals(account.__class__.__name__, 'Account')
+        self.assertEquals(account._url, 'Accounts/' + self.client.account_sid)
+        self.assertEquals(account.sid, self.client.account_sid)
+        self.assertEquals(account.status, 'active')
+
+    def test_notifications_list_resource(self):
+        account = self.client.accounts[self.client.account_sid]
+        notifications = account.notifications
+        self.assertEquals(notifications.__class__.__name__, 'NotificationListResource')
+
+        # len()
+        self.assertTrue(len(notifications))
+
+        # Negative Indexes
+        notifications[-1]
+
+        # Iteration
+        for notification in notifications:
+            notification.request_url
+
+        # Slicing
+        for notification in notifications[10:20]:
+            notification.request_url
+
+        for notification in notifications[1:5:-1]:
+            notification.request_url
+
+        # Disallow setattr
+        with self.assertRaises(TypeError):
+            notifications[0] = 5
+
+    def test_pagination(self):
+        test_dir = os.path.dirname(__file__)
+        accounts_data = json.load(open(os.path.join(test_dir, 'test_accounts.json')))
+
+        # Internal call to use dummy data
+        accounts = self.client.accounts[10:20]
+        accounts.fetch(resource_data=accounts_data)
+
+        for i, account in enumerate(accounts):
+            self.assertIsNotNone(account.sid)
+
+        self.assertEquals(i, 9)
+        
+        # Internal call to use dummy data
+        accounts = self.client.accounts[20:40]
+        accounts.fetch(resource_data=accounts_data)
+
+        for i, account in enumerate(accounts):
+            self.assertIsNotNone(account.sid)
+
+        self.assertEquals(i, 19)
+
+    def test_sms_list(self):
+        sms_list = self.client.accounts[self.client.account_sid].sms_messages[:100]
+
+        for i, sms in enumerate(sms_list):
+            self.assertTrue(sms.body)
+            self.assertTrue(sms.from_number)
+            self.assertTrue(sms.to_number)
+
+        self.assertEquals(i + 1, len(sms_list))
+
+    def test_sms_send(self):
+        sms_list = self.client.accounts[self.client.account_sid].sms_messages
+        from_number = to_number = self.test_cell_number
+        body = "Hello from telapi-python!"
+        sms = sms_list.create(from_number=from_number, to_number=to_number, body=body)
+        self.assertTrue(sms.sid.startswith('SM'))
+        self.assertEquals(sms.body, body)
+        self.assertEquals(sms.from_number, from_number)
+        self.assertEquals(sms.to_number, to_number)
+
+    def test_incoming_phone_numbers(self):
+        account = self.client.accounts[self.client.account_sid]
+
+        # Test listing
+        for number in account.incoming_phone_numbers:
+            self.assertTrue(number.phone_number.startswith('+'))
+
+
+        # Make sure there's no exception when there's no results
+        no_available_numbers = account.available_phone_numbers.filter(AreaCode=999)
+        # self.assertEquals(len(no_available_numbers), 0)
+
+        for number in no_available_numbers:
+            print no_available_numbers.phone_number
+
+
+        # Filter and buy a DID
+        available_numbers = account.available_phone_numbers.filter(AreaCode=435)
+        self.assertTrue(len(available_numbers))
+
+        for number in available_numbers:
+            self.assertEquals(number.phone_number[:5], '+1435')
+            self.assertEquals(number.npa, "435")
+            self.assertEquals(number.country_code, "+1")
+
+        good_available_number = available_numbers[0]
+        phone_number = good_available_number.phone_number
+        purchase_number = account.incoming_phone_numbers.create(phone_number=phone_number)
+
+
+        # Try to update
+        purchase_number.voice_url = voice_url = "http://db.tt/YtLJgpa8"
+        purchase_number.save()
+
+
+        # Make sure the number is in the list (clear the cache with .clear first)
+        found = False
+        
+        for number in account.incoming_phone_numbers.clear():
+            if number.phone_number == phone_number and number.voice_url.strip() == voice_url:
+                found = True
+                break
+
+        self.assertTrue(found)
+
+
+        # Delete the number
+        purchase_number.delete()
+
+
+        # Make sure the number is no longer in the list
+        found = False
+        for number in account.incoming_phone_numbers:
+            if number.phone_number == phone_number and number.voice_url == voice_url:
+                found = True
+                break
+
+        self.assertTrue(not found)
+
+    def test_calls(self):
+        account = self.client.accounts[self.client.account_sid]
+
+        # Typecast to list to make sure enumeration works
+        list(account.calls)
+
+        # Use alternate syntax to create to update properties before saving
+        call = account.calls.new()
+        call.from_number = "+17328381916"
+        call.to_number = "+17328381916"
+        call.url = "https://dl.dropbox.com/u/14573179/TML/wait_music.xml"
+
+        # Dial
+        call.save()
+
+        # Wait a bit
+        time.sleep(10)
+
+        # Hangup
+        call.status = "Completed"
+        call.save()
+
+
+if __name__ == '__main__':
+    unittest.main()
